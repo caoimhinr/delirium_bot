ce20b9bc1cb538c4b808e23fb1c1ef800c018b36
caoimhinr <kevin@welvaert.org>
2025-11-25 22:06:11 +0100

    implement command operator const

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
+const COMMAND_OPERATOR = '$'
+
 client.once('clientReady', () => {
     console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on('messageCreate', message => {
-    if(message.content === '!ping') {
+    if(message.content === `${COMMAND_OPERATOR}ping`) {
         message.channel.send('Pong!');
     }
 });
