7c38a4f5a5607b346a3184381b272e3358b86404
Kevin Welvaert ATA IT <kwe@atait.com>
2026-03-12 14:06:24 +0100

    feat(claiming): claiming flow + maintenance

--- discordClient.js
+++ discordClient.js
@@ -1,11 +1,17 @@
 // discordClient.js
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
     GatewayIntentBits.MessageContent,
+  ],
+  partials: [
+    Partials.Message,
+    Partials.Channel,
+    Partials.Reaction,
   ],
 });
 
